7945e7c0725f9142ec263f48a6c5b2e12f9a6327
Tobias Hvass Mølbak <tobias@molbak.org>
2017-05-21 13:13:32 +0200

    Now gathers information from TheMovieDB based on name query

--- app/src/main/java/molbak/showcaseproject/MainActivity.java
+++ app/src/main/java/molbak/showcaseproject/MainActivity.java
@@ -1,6 +1,10 @@
 package molbak.showcaseproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +19,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -24,8 +39,6 @@ public class MainActivity extends AppCompatActivity
 
     private Button btnAddMovie;
     private EditText txtTitle;
-    private EditText txtDescription;
-    private EditText txtYear;
 
     public DatabaseHandler database = null;
 
@@ -45,6 +58,9 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         database = new DatabaseHandler(this);
+
+        //moviesDB = new TmdbApi(getResources().getString(R.string.tmdb_api)).getMovies();
+        //MovieDb movieDb = moviesDB.getMovie()
 
         initiateliseUI();
 
@@ -79,17 +95,12 @@ public class MainActivity extends AppCompatActivity
 
     private void initiateliseUI() {
         txtTitle = (EditText) findViewById(R.id.txt_title);
-        txtDescription = (EditText) findViewById(R.id.txt_description);
-        txtYear = (EditText) findViewById(R.id.txt_year);
 
         btnAddMovie = (Button) findViewById(R.id.btn_add_movie);
         btnAddMovie.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Movie movie = new Movie();
-                if (txtTitle.getText().toString().equals("") ||
-                        txtDescription.getText().toString().equals("") ||
-                        txtYear.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+                if (txtTitle.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "No name stated", Toast.LENGTH_SHORT).show();
 
                     List<Movie> movieList = database.getAllMovies();
                     for (Movie mov : movieList) {
@@ -101,19 +112,8 @@ public class MainActivity extends AppCompatActivity
                     }
 
                 } else {
-                    try {
-                        movie.setTitle(txtTitle.getText().toString());
-                        movie.setDescription(txtDescription.getText().toString());
-                        movie.setYear(Integer.parseInt(txtYear.getText().toString()));
-                        database.addMovie(movie);
-
-                        txtTitle.setText("");
-                        txtDescription.setText("");
-                        txtYear.setText("");
-                        Toast.makeText(getApplicationContext(), "Movie added", Toast.LENGTH_SHORT).show();
-                    } catch (NumberFormatException e) {
-                        Toast.makeText(getApplicationContext(), "Invalid year stated", Toast.LENGTH_SHORT).show();
-                        txtYear.setText("");
+                    if (isOnline()) {
+                        new TMDBQueryManager().execute(txtTitle.getText().toString());
                     }
                 }
             }
@@ -122,13 +122,118 @@ public class MainActivity extends AppCompatActivity
 
     private void initiateliseDatabase() {
         Log.i(TAG, "Initialising Database");
-        database.addMovie(new Movie("Guardians of the Galaxy 2", "Science Fiction", 2017));
-        database.addMovie(new Movie("Pulp Fiction", "Crime Drama", 1994));
-        database.addMovie(new Movie("The Empire Strikes Back", "Fantasy", 1980));
-        database.addMovie(new Movie("Forrest Gump", "Comedy Romance", 1994));
-        database.addMovie(new Movie("Inception", "Adventure", 2010));
-        database.addMovie(new Movie("The Dark Knight", "Action", 2008));
-        database.addMovie(new Movie("The Shawshank Redemption", "Crime Drama", 1994));
-        database.addMovie(new Movie("The Return of the King", "Adventure Fantasy", 2003));
+        new TMDBQueryManager().execute("Pulp Fiction");
+        new TMDBQueryManager().execute("Return of the King");
+        new TMDBQueryManager().execute("Forrest Gump");
+        new TMDBQueryManager().execute("Shawshank");
+        new TMDBQueryManager().execute("Empire Strikes Back");
+
+        // database.addMovie(new Movie("Guardians of the Galaxy 2", "Science Fiction", 2017));
+        // database.addMovie(new Movie("Pulp Fiction", "Crime Drama", 1994));
+        // database.addMovie(new Movie("The Empire Strikes Back", "Fantasy", 1980));
+        // database.addMovie(new Movie("Forrest Gump", "Comedy Romance", 1994));
+        // database.addMovie(new Movie("Inception", "Adventure", 2010));
+        // database.addMovie(new Movie("The Dark Knight", "Action", 2008));
+        // database.addMovie(new Movie("The Shawshank Redemption", "Crime Drama", 1994));
+        // database.addMovie(new Movie("The Return of the King", "Adventure Fantasy", 2003));
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private class TMDBQueryManager extends AsyncTask<String, Void, Movie> {
+
+        private final String TMDB_API_KEY = getResources().getString(R.string.tmdb_api);
+        private static final String DEBUG_TAG = "TMDBQueryManager";
+
+        @Override
+        protected Movie doInBackground(String... params) {
+            try {
+                Log.i(DEBUG_TAG, "Do in background");
+                return searchIMDB(params[0]);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Movie movie) {
+            Log.i(DEBUG_TAG, "PostExecute");
+            if (movie != null) {
+                Log.i(DEBUG_TAG, "Adding movie to DB");
+                database.addMovie(movie);
+                Toast.makeText(getApplicationContext(), "Movie added", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Movie NOT added", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        /**
+         * Searches IMDBs API for the given query
+         * @param query The query to search.
+         * @return A list of all hits.
+         */
+        public Movie searchIMDB(String query) throws IOException {
+            // Build URL
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("http://api.themoviedb.org/3/search/movie");
+            stringBuilder.append("?api_key=" + TMDB_API_KEY);
+            stringBuilder.append("&query=" + query.replaceAll(" ", "+"));
+            URL url = new URL(stringBuilder.toString());
+
+            InputStream stream = null;
+            try {
+                // Establish a connection
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.addRequestProperty("Accept", "application/json"); // Required to get TMDB to play nicely.
+                conn.setDoInput(true);
+                conn.connect();
+
+                int responseCode = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response code is: " + responseCode + " " + conn.getResponseMessage());
+
+                stream = conn.getInputStream();
+                return parseResult(stringify(stream));
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
+        }
+
+        private Movie parseResult(String result) {
+            String streamAsString = result;
+            Movie movie = new Movie();
+            try {
+                JSONObject jsonObject = new JSONObject(streamAsString);
+                JSONArray array = (JSONArray) jsonObject.get("results");
+
+                JSONObject jsonMovieObject = array.getJSONObject(0);
+                movie.setTitle(jsonMovieObject.getString("original_title"));
+                movie.setDescription(jsonMovieObject.getString("overview"));
+
+                String releaseYear = jsonMovieObject.getString("release_date");
+                String[] releaseYearParts = releaseYear.split("-");
+                movie.setYear(Integer.parseInt(releaseYearParts[0]));
+
+            } catch (JSONException e) {
+                System.err.println(e);
+                Log.d(DEBUG_TAG, "Error parsing JSON. String was: " + streamAsString);
+            }
+            return movie;
+        }
+
+        public String stringify(InputStream stream) throws IOException {
+            Reader reader = new InputStreamReader(stream, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            return bufferedReader.readLine();
+        }
     }
 }
